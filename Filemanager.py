b740788d334ee63164230c7ac3f111a4fdb3ae1d
MAM <muzammilmohd538@gmail.com>
2022-11-05 16:19:49 +0530

    init

---
+++ Filemanager.py
@@ -0,0 +1,14 @@
+import os , shutil
+
+path = input("Enter Path :\t")
+files = os.listdir(path)
+
+for file in files:
+    name , ext = os.path.splitext(file)
+    ext = ext[1:]
+
+    if os.path.exists(path+'/'+ext):
+        shutil.move(path+'/'+file , path+'/'+ext+'/'+file)
+    else:
+        os.mkdir(path+'/'+ext)
+        shutil.move(path+'/'+file , path+'/'+ext+'/'+file)
